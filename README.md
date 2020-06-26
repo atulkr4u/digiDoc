4afb077d1af9d53016f28a95fb9f1259b87a2d13
atulrastogipb <atulrastogi@policybazaar.com>
2020-06-26 10:25:05 +0530

    Init App Able to Save Data to FireBase

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# digiDoc
-App where you can add a digital page for anything, add reminders, add tags &amp; more to make these doc work for you
+Digital Document
---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import firebase from "firebase";
+
+const config  = {
+    apiKey: "AIzaSyAnhiB_wxoh--be-bEt9Z9T5BpciAiyhbM",
+    authDomain: "hellofirebase-d5b99.firebaseapp.com",
+    databaseURL: "https://hellofirebase-d5b99.firebaseio.com",
+    projectId: "hellofirebase-d5b99",
+    storageBucket: "hellofirebase-d5b99.appspot.com",
+    messagingSenderId: "860592191798",
+    appId: "1:860592191798:web:ce4775af43033be8bf1b97"
+  };
+// Initialize Firebase
+firebase.initializeApp(config);
+
+export default firebase;
